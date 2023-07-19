661b680d84e26c6393d375a679c59856ecdd6f25
isamurataliev <isawalker2009@gmail,com>
2023-07-19 20:03:01 +0600

    Initial commit

---
+++ src/com/company/Skeleton.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Skeleton extends Boss{
+    private int numbersOfArrows;
+
+    public int getNumbersOfArrows() {
+        return numbersOfArrows;
+    }
+
+    public void setNumbersOfArrows(int numbersOfArrows) {
+        this.numbersOfArrows = numbersOfArrows;
+    }
+
+    public String info (){
+        return super.info() + ", Numbers of arrows: " + this.numbersOfArrows;
+    }
+}
